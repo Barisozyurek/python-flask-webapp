3d0b175827f16ccfa80a4316d11fc193e0809d8c
Barisozyurek <Barisozyurek@users.noreply.github.com>
2024-01-07 02:57:25 +0300

    Password generator tool added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
+import random
+import string
 
 app = Flask(__name__)
 
@@ -11,6 +13,40 @@ def index():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+# Tools Page
+@app.route('/tools')
+def tools():
+    return render_template('tools.html')
+
+# Password Generator Page
+@app.route('/password-generator', methods=['GET', 'POST'])
+def password_generator():
+    if request.method == 'POST':
+        length = int(request.form.get('length', 12))
+        use_lowercase = 'use_lowercase' in request.form
+        use_uppercase = 'use_uppercase' in request.form
+        use_digits = 'use_digits' in request.form
+        use_special_characters = 'use_special_characters' in request.form
+
+        password = generate_random_password(length, use_lowercase, use_uppercase, use_digits, use_special_characters)
+        return render_template('password-generator.html', password=password, length=length, use_lowercase=use_lowercase, use_uppercase=use_uppercase, use_digits=use_digits, use_special_characters=use_special_characters)
+
+    password = generate_random_password(12, 1, 1, 1, 1)
+    return render_template('password-generator.html', password=password, length=12, use_lowercase=True, use_uppercase=True, use_digits=True, use_special_characters=True)
+
+
+def generate_random_password(length=12, use_lowercase=True, use_uppercase=True, use_digits=True, use_special_characters=True):
+    characters = string.ascii_lowercase if use_lowercase else ''
+    characters += string.ascii_uppercase if use_uppercase else ''
+    characters += string.digits if use_digits else ''
+    characters += string.punctuation if use_special_characters else ''
+
+    if not characters:
+        return "Please select at least one character type."
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
 
 # Twitter redirect
 @app.route('/twitter')
